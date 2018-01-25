893810b120fd94b765782e71a9898b407b5d170f
erika <erika@serox.si>
2018-01-25 14:14:35 +0100

    Cleanup

--- contracts/CFIToken.sol
+++ contracts/CFIToken.sol
@@ -6,8 +6,8 @@ contract CFIToken is Token {
 
   /* Initializes contract */
   function StormToken(address _crowdsaleAddress, uint _tokenStartBlock) {
-    standard = "CFI Token v1.0";
-    name = "CFI Token";
+    standard = " v1.0"; // Set token standard
+    name = ""; // Set token name
     symbol = ""; //TODO: set token symbol
     decimals = 18;
     crowdsaleContractAddress = _crowdsaleAddress;
