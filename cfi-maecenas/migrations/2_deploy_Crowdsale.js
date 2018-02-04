9b8d7b2a15ced11ec168d790d2290f86e32975ca
erika <erika@serox.si>
2018-02-04 13:06:45 +0100

    cleanup

--- cfi-maecenas/migrations/2_deploy_Crowdsale.js
+++
@@ -1,5 +0,0 @@
-var Crowdsale = artifacts.require("./MaecenasCrowdsale.sol"); // For testing use this .sol
-
-module.exports = function(deployer) {
-  deployer.deploy(Crowdsale);
-};
--- cfi-maecenas/migrations/3_deploy_Token.js
+++
@@ -1,9 +0,0 @@
-var Crowdsale = artifacts.require("./MaecenasCrowdsale.sol");   //For testing use this .sol
-var Token = artifacts.require("./MaecenasToken.sol");           // For testing use this .sol
-var SafeMathLib = artifacts.require("./Utils/SafeMath.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(SafeMathLib);
-  deployer.link(SafeMathLib, Token);
-  deployer.deploy(Token, Crowdsale.deployed().then(function(crowdsaleInsance){return crowdsaleInsance.address}), 4366494);
-};
