f857ccf7d825683c269cb9cc716a4ab803342af9
Erika Pogorelc <erika@serox.si>
2018-02-18 15:59:57 +0100

    Fixes values

--- cfi-futourist/contracts/FutouristCrowdsale.sol
+++ cfi-futourist/contracts/FutouristCrowdsale.sol
@@ -3,21 +3,23 @@ pragma solidity ^0.4.13;
 import "./Crowdsale.sol";
 
 contract FutouristCrowdsale is Crowdsale {
-  function FutouristCrowdsale(){
-    presaleStartBlock; //TODO: set block
-    presaleUnlimitedStartBlock; //TODO: set block
-    crowdsaleStartBlock; //TODO: set block
-    crowdsaleEndedBlock; //TODO: set block
+  function FutouristCrowdsale() {
+    /* ADAPT */
+    presaleStartTime = 1519142400; //20/2/2017/1700
+    presaleUnlimitedStartTime = 1519401600; //23/2/2017/1700
+    crowdsaleStartTime = 1519747200; //27/2/2017/1700
+    crowdsaleEndedTime = 1521561600; //20/3/2017/1700
 
-    minCap; //TODO: set minCap
-    maxP1Cap; //TODO: set maxP1Cap
-    maxCap; //TODO: set maxCap
+    minCap = 1 ether;
+    maxCap = 5226 ether;
+    maxP1Cap = 5226 ether;
 
-    ethToTokenConversion; //TODO: set conversion
+    ethToTokenConversion = 45938; //TODO: set conversion
 
-    maxTokenSupply; //TODO: set supply
-    presaleBonusTokens; //TODO: set number of tokens set for the presale bonuses
+    maxTokenSupply = 1000000000 ether; //TODO: set supply
+    presaleBonusTokens = 109929634 ether; //TODO: set number of tokens set for the presale bonuses
     presaleBonusAddress; // TODO: set the address for the presale bonuses tokens
     presaleBonusAddressColdStorage; // TODO: set the cold storage address for the presale bonuses tokens
+    /* /ADAPT */
   }
 }
