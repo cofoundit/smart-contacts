0b91e5cfc05625f5216373a52d83cdb31a697b8e
Erika Pogorelc <erika@serox.si>
2018-02-20 10:14:31 +0100

    Update values

--- cfi-futourist/contracts/FutouristToken.sol
+++ cfi-futourist/contracts/FutouristToken.sol
@@ -5,12 +5,12 @@ import "./Token.sol";
 contract FutouristToken is Token {
 
   /* Initializes contract */
-  function FutouristToken(address _crowdsaleAddress, uint _tokenStartBlock) {
-    standard = " v1.0"; // Set token standard
-    name = "FutouristToken"; // Set token name
-    symbol = ""; //TODO: set token symbol
+  function FutouristToken(address _crowdsaleAddress, uint _tokenStartTime) {
+    standard = " v1.0";
+    name = "FutouristToken";
+    symbol = "FTR";
     decimals = 18;
     crowdsaleContractAddress = _crowdsaleAddress;
-    lockFromSelf(_tokenStartBlock, "Lock before crowdsale starts");
+    lockFromSelf(_tokenStartTime, "Lock before crowdsale starts");
   }
 }
