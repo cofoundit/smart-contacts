088a1dccc686be252aae46ac211383fca49d1044
Erika Pogorelc <erika@serox.si>
2018-02-20 08:45:29 +0100

    Updates scope of function

--- cfi-futourist/contracts/Interfaces/IToken.sol
+++ cfi-futourist/contracts/Interfaces/IToken.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.13;
 
 contract IToken {
-  function totalSupply() constant returns (uint256 totalSupply);
-  function mintTokens(address _to, uint256 _amount) {}
+  function totalSupply() public constant returns (uint256 totalSupply);
+  function mintTokens(address _to, uint256 _amount) public {}
 }
